79a6c39f0e0d14a0f65c6acb661a3ceee6e337c0
Ra1v00 <79638847+Ra1v00@users.noreply.github.com>
2023-10-04 14:21:58 +0700

    Forge 1.0
    Ful animation of taking AL

---
+++ My project/Assets/Scripts/Act_4.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Action_4 : MonoBehaviour
+{
+    public Transform EndPoint;
+
+    private Transform currentTarget = null; // Текущий целевой EndPoint
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha4) && currentTarget == null)
+        {
+            currentTarget = EndPoint;
+        }
+
+        if (currentTarget != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, currentTarget.position, Time.deltaTime);
+
+            // Если достигли целевой точки
+            if (Vector3.Distance(transform.position, currentTarget.position) < 0.001f)
+            {
+                currentTarget = null; // Останавливаем движение
+            }
+        }
+    }
+}
--- My project/Assets/Scripts/Animation.cs
+++ My project/Assets/Scripts/Animation.cs
@@ -3,33 +3,39 @@ using UnityEngine;
 
 public class DoorRotation : MonoBehaviour
 {
-    private bool isOpening = false;
+    private bool isMoving = false; // Вместо только открывания мы учитываем оба направления
+    private bool isOpen = false; // Для проверки текущего состояния двери (открыта/закрыта)
     private float startAngle = 0f;
-    private float targetAngle = 90f;
-    private float duration = 2f; // Продолжительность анимации в секундах
+    private float openAngle = 90f; // угол, при котором дверь считается открытой
+    private float duration = 2f;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && !isOpening)
+        // При нажатии на кнопку и если дверь не двигается
+        if (Input.GetMouseButtonDown(0) && !isMoving)
         {
-            isOpening = true;
-            StartCoroutine(OpenDoor());
+            StartCoroutine(ToggleDoor());
         }
     }
 
-    IEnumerator OpenDoor()
+    IEnumerator ToggleDoor()
     {
+        isMoving = true;
+
         float elapsedTime = 0f;
+        float initialAngle = transform.localEulerAngles.z;
+        float targetAngle = isOpen ? startAngle : openAngle;
 
         while (elapsedTime < duration)
         {
-            float currentAngle = Mathf.Lerp(startAngle, targetAngle, elapsedTime / duration);
+            float currentAngle = Mathf.Lerp(initialAngle, targetAngle, elapsedTime / duration);
             transform.localEulerAngles = new Vector3(0f, 0f, currentAngle);
             elapsedTime += Time.deltaTime;
             yield return null;
         }
 
         transform.localEulerAngles = new Vector3(0f, 0f, targetAngle);
-        isOpening = false;
+        isOpen = !isOpen; // Меняем состояние двери
+        isMoving = false;
     }
 }
